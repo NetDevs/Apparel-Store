f4b61dc49c99a4fe74f97102bec6e2d8237310ee
BhumikaTomar <zoya1994@gmail.com>
2018-10-08 12:41:01 +0530

    Intial git commit

---
+++ ApparelStore/ApparelStoreWebService/Components/ErrorMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApparelStoreWebService.Components
+{
+    public class ExceptionHandlingServiceMiddleware
+    {
+        public RequestDelegate Next;
+        public ExceptionHandlingServiceMiddleware(RequestDelegate Next)
+        {
+            this.Next = Next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await Next.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                context.Response.ContentType = "application/json";
+                
+                await context.Response.WriteAsync(e.Message);
+                
+            }
+        }
+    }
+}
