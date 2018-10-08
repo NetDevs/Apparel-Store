c65427aa0417935f484b24619ff8ff35160b251c
BhumikaTomar <zoya1994@gmail.com>
2018-10-08 16:54:57 +0530

    commit changes from vidushi

--- ApparelStore/ApparelStoreApplication/Program.cs
+++ ApparelStore/ApparelStoreApplication/Program.cs
@@ -20,5 +20,7 @@ namespace ApparelStoreApplication
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        //abcd
     }
 }
